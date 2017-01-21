b6a4f7e2d9068a06e8fd8a5b658d5a854d9adc11
Emilio Gozo <emilio.gozo@obf.ateneo.edu>
2017-01-22 00:34:43 +0800

    feat: Navigate to EmployeeCreate form

--- src/Router.js
+++ src/Router.js
@@ -1,7 +1,8 @@
 import React from 'react';
-import { Scene, Router } from 'react-native-router-flux';
+import { Scene, Router, Actions } from 'react-native-router-flux';
 import LoginForm from './components/LoginForm';
 import EmployeeList from './components/EmployeeList';
+import EmployeeCreate from './components/EmployeeCreate';
 
 const RouterComponent = () => {
   return (
@@ -16,9 +17,17 @@ const RouterComponent = () => {
 
       <Scene key="main">
         <Scene
+          onRight={() => Actions.employeeCreate()}
+          rightTitle="Add"
           key="employeeList"
           component={EmployeeList}
           title="Employees"
+          initial
+        />
+        <Scene
+          key="employeeCreate"
+          component={EmployeeCreate}
+          title="Create Employee"
         />
       </Scene>
     </Router>
