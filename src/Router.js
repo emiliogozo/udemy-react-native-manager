345b749881fe3c2dfc90bae1b86235d689f90e7e
Emilio Gozo <emilio.gozo@obf.ateneo.edu>
2017-01-21 23:51:16 +0800

    feat: Added EmployeeList scene and routing

--- src/Router.js
+++ src/Router.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Scene, Router } from 'react-native-router-flux';
 import LoginForm from './components/LoginForm';
+import EmployeeList from './components/EmployeeList';
 
 const RouterComponent = () => {
   return (
@@ -9,6 +10,11 @@ const RouterComponent = () => {
         key="login"
         component={LoginForm}
         title="Please Login"
+      />
+      <Scene
+        key="employeeList"
+        component={EmployeeList}
+        title="Employees"
       />
     </Router>
   );
